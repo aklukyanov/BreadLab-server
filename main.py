524e3892203c1e0386e90258ab5ff8f129e6766c
Alexey <aklukyanov@gmail.com>
2026-03-19 21:51:15 +0300

    basic project structure

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from breadlab-api!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ utils/client.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_dotenv
+from ollama import Client
+
+load_dotenv()
+
+ollama_api_key=os.getenv('ollama_api')
+cloud_client = Client(
+    host='https://ollama.com',
+    headers={'Authorization': f'Bearer {ollama_api_key}'}
+)
