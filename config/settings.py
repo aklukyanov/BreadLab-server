be702afee9bec7dca7e1191c69802b7f70381708
Alexey <aklukyanov@gmail.com>
2026-07-11 14:19:08 +0300

    + фикс настройки окружений: подключение бд через настройку .env

--- config/settings.py
+++ config/settings.py
@@ -78,25 +78,27 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-# Для разработки
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+# Database
+DB_ENGINE = os.getenv('DB', 'sqlite').lower()
+
+if DB_ENGINE == 'postgres':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'breadlab-db',
+            'USER': os.getenv("POSTGRES_USER"),
+            'PASSWORD': os.getenv("POSTGRES_PASSWORD"),
+            'HOST': os.getenv("DB_HOST", "breadlab-db"),
+            'PORT': os.getenv("DB_PORT", "5432"),
+        }
     }
-}
-
-# ДЛЯ ПРОДА!
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'breadlab-db',
-#         'USER': os.getenv("POSTGRES_USER"),
-#         'PASSWORD': os.getenv("POSTGRES_PASSWORD"),
-#         'HOST': 'breadlab-db',
-#         'PORT': '5432',
-#     }
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
